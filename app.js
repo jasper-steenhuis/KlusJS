715bd8a385513f47230afc85c7fe685d46892aa9
jasper-steenhuis <steenhuisjasper1@gmail.com>
2021-12-01 11:42:13 +0100

    tying objects to html functionality

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 /**
  * function that appends task to task list
  */
+var tasks = [];
 function appendToList() {
     let input = document.getElementById("task-name-input");
     if(input.value != ""){
@@ -8,11 +9,14 @@ function appendToList() {
         let node = document.createElement("LI");
         let textNode = document.createTextNode(input.value + " ");
         node.appendChild(textNode);
-    
-        addButtonToTask(node);
+        var taskObject = {desc: textNode, markup: node, priority: true};
+
+        addButtonToTask(taskObject);
         taskList.appendChild(node);
     
+        tasks.push(taskObject);
         input.value = " ";
+        console.log(taskObject.desc.textContent + " " + taskObject.priority);
     }else{
         alert("Task name must not be empty!");
     }
@@ -23,8 +27,13 @@ function addButtonToTask(task){
     let buttonElement = document.createElement("BUTTON");
     buttonElement.innerHTML = "-";
 
-    task.appendChild(buttonElement);
+    task.markup.appendChild(buttonElement);
     buttonElement.onclick = function() {
-        task.style.setProperty('text-decoration', 'line-through');
+        task.markup.style.setProperty('text-decoration', 'line-through');
+        task.priority = false;
+        sortToBottom(task);
     }
+}
+function sortToBottom(task){
+    console.log(task.desc.textContent + " " + task.priority);
 }
