bbe6b5e4378db385e7e780087c7afa45b1353cb3
jasper-steenhuis <steenhuisjasper1@gmail.com>
2021-12-02 11:41:26 +0100

    refactoring

--- app.js
+++ app.js
@@ -2,38 +2,38 @@
  * function that appends task to task list
  */
 var tasks = [];
-function appendToList() {
+function createObjectAndAppendHTML() {
     let input = document.getElementById("task-name-input");
-    if(input.value != ""){
-        let taskList = document.getElementById("task-list");
-        let node = document.createElement("LI");
-        let textNode = document.createTextNode(input.value + " ");
-        node.appendChild(textNode);
-        var taskObject = {desc: textNode, markup: node, priority: true};
-
-        addButtonToTask(taskObject);
-        taskList.appendChild(node);
-    
+    if (input.value != "") {
+        var taskObject = { desc: input.value, markup: " ", priority: false };
         tasks.push(taskObject);
         input.value = " ";
-        console.log(taskObject.desc.textContent + " " + taskObject.priority);
-    }else{
+        appendToHTML(taskObject);
+        addButtonToTask(taskObject);
+    } else {
         alert("Task name must not be empty!");
     }
-    
-
 }
-function addButtonToTask(task){
+function addButtonToTask(task) {
     let buttonElement = document.createElement("BUTTON");
     buttonElement.innerHTML = "-";
-
     task.markup.appendChild(buttonElement);
-    buttonElement.onclick = function() {
+    buttonElement.onclick = function () {
         task.markup.style.setProperty('text-decoration', 'line-through');
-        task.priority = false;
-        sortToBottom(task);
+        removeFromList(task);
     }
 }
-function sortToBottom(task){
-    console.log(task.desc.textContent + " " + task.priority);
+function removeFromList(task) {
+    var index = tasks.indexOf(task);
+    if (index > -1) {
+        tasks.splice(index, 1);
+    }
+    console.log(tasks);
+}
+function appendToHTML(task) {
+    let taskListHTML = document.getElementById("task-list");
+    task.markup = document.createElement("LI");
+    let textNode = document.createTextNode(task.desc);
+    task.markup.appendChild(textNode);
+    taskListHTML.appendChild(task.markup);
 }
